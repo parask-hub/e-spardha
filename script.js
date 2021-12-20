278d5463f8a5a45a35013ca385710fd725bf92bc
Paraskhilosiya <95310241+Paraskhilosiya@users.noreply.github.com>
2021-12-20 09:55:46 +0530

    quiz tab

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function classToggle() {
-    const navs = document.querySelectorAll('.Navbar_items')
-    navs.forEach(nav => nav.classList.toggle('.Navbar_ToggleShow'));
-
-
-}
-
-document.querySelector('.Navbar_Link-Toggle')
-    .addEventListener('click', classToggle);
+let name = prompt("Name :");

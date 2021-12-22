daaca0cee0e6b2fecd183b297361e3b21f8170d4
Paraskhilosiya <95310241+Paraskhilosiya@users.noreply.github.com>
2021-12-22 22:56:41 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let name = prompt("Name :");
+// FOR RESPONSIVE NAVBAR
+function classToggle() {
+            const navs = document.querySelectorAll('.navbar_items')
+
+            navs.forEach(nav => nav.classList.toggle('navbar_toggleshow'));
+        }
+        document.querySelector('.navbar_link-toggle')
+            .addEventListener('click', classToggle)
+// RESPONSIVE NAVBAR CODE ENDS
+ // Mathematical tool Button
+ 
+ 
